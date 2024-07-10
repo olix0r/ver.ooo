4216b9d0e9fe49e5639676ac93439eebf173497d
Oliver Gould <ver@buoyant.io>
2024-07-10 05:08:18 +0000

    fix random strategy selection

--- src/lib/strategy.ts
+++ src/lib/strategy.ts
@@ -197,15 +197,25 @@ const strategies = [
   'Your mistake was a hidden intention',
 ];
 
-function seed(): number {
+function stableSeed(): number {
   const date = new Date();
   return Math.floor(date.getTime() / (15 * 60 * 1000));
 }
 
+function prng(seed: number): number {
+  const x = Math.sin(seed) * 10000;
+  return x - Math.floor(x);
+}
+
+/// Returns a random number between 0 and strategies.length, that will be stable for 15 minutes.
+function stableRandomStrategy(): string {
+  const idx = Math.floor(prng(stableSeed()) * strategies.length);
+  return strategies[idx];
+}
+
 export function strategy(idx?: number): string {
   if (idx === undefined) {
-    idx = seed();
+    return stableRandomStrategy();
   }
-  const strategy = strategies[idx % strategies.length];
-  return strategy;
+  return strategies[idx % strategies.length];
 }
